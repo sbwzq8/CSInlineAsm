9b9823e970011134477c394f558c1a35d26f1958
sbwzq8 <sbwzq8@mail.missouri.edu>
2018-12-29 23:32:43 -0600

    Fixed typo

--- compile.bat
+++ compile.bat
@@ -3,4 +3,4 @@ cl /c /MD nativecode.cpp
 cl /clr /LN /MD clrcode.cpp nativecode.obj
 csc /target:module /addmodule:clrcode.netmodule Program.cs Properties\AssemblyInfo.cs
 link /LTCG /CLRIMAGETYPE:IJW /ENTRY:testcpp.Program.Main /SUBSYSTEM:CONSOLE /ASSEMBLYMODULE:clrcode.netmodule /OUT:InlineASM.exe clrcode.obj nativecode.obj program.netmodule
-echo Finished compile. Run with MixedApp.exe
+echo Finished compile. Run with InlineASM.exe
